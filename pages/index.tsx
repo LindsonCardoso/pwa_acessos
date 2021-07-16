3980d68cfc9640ac57d4e59572d5702ad1d493c3
unknown <lindsoncardoso.al@gmail.com>
2021-07-16 09:57:26 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import ListAcess from '../src/components/ListAcess';
+
+ const Index = () => {
+  return (
+      <div>
+          <ListAcess/>
+      </div>
+  )
+}
+
+
+export default Index;
---
+++ src/components/ListAcess.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from '../../src/styles/Home.module.css'
+
+ const ListAcess = () => {
+  return (
+    <div className={styles.container}>
+          <h1 className={styles.title}>
+         Acessos
+        </h1>
+      <main className={styles.main}>
+      
+      </main>
+ 
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  )
+}
+
+
+export default ListAcess;
